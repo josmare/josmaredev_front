d9c0680e9c3d439a7b09b682cc6fde2d1ca2d2e4
Josmar <josmar.regalado@gmail.com>
2023-02-07 22:21:00 +0200

    First complete version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,30 +11,38 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+  <h2> My name is Josmar Regalado </h2>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    I am an software developer currently based in Helsinki.
+    I  mainly build solutions with
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue</a>
+    +
+    <a href="https://www.djangoproject.com/" target="_blank" rel="noopener">Django</a>
+    living on
+    <a href="https://kubernetes.io/" target="_blank" rel="noopener">Kubernetes</a>
+     clusters.
   </p>
   <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
+    Outside of programming I
+    <a href="https://twitter.com/josmarregalado" target="_blank" rel="noopener">tweet</a>
+     random things and pilot drones. You can find some shots on my
+    <a href="https://www.instagram.com/josmar.regalado/" target="_blank" rel="noopener">instagram</a>.
+    If I have time left, I practice bouldering, 🏂, and do 🧖-avanto (❄🏊).
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
 }
+
+p {
+  font-size: 1.6em;
+  font-weight: 300;
+  line-height: 1.4;
+  max-width: 28em;
+}
+
 </style>
